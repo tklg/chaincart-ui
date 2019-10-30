8507db900eb48654abeaee981609e7bd412fcdad
unknown <villa7.kluge5@gmail.com>
2019-10-29 23:18:39 -0400

    fix: globals

--- src/ErrorBoundary/index.js
+++ src/ErrorBoundary/index.js
@@ -12,7 +12,7 @@ export default class ErrorBoundary extends React.Component {
   static getDerivedStateFromError (error) {
     // Update state so the next render will show the fallback UI.
     return {
-      error: location.hostname === 'localhost' ? error : ''
+      error: window.location.hostname === 'localhost' ? error : ''
     }
   }
 
